20c648eb48248ea07658b790a4c706ce45b4e901
Jonathan Chen <ethan.chen@covenant.edu>
2020-07-09 11:16:15 -0400

    create contact function

--- createContact.js
+++ createContact.js
@@ -1,12 +1,12 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.handler = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: `Hello, World! ${event.queryStringParameters.name}`,
+        input: `Hello, World! ${event.body}`,
       },
       null,
       2
